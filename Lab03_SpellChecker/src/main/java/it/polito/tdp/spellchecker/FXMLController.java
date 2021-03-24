340b9764fbaa17b9e6c774a69ac60c486cf731a5
utente <utente@LAPTOP-L0TNJ455.station>
2021-03-24 17:13:05 +0100

    Soluzione es 1 lab03, errore che stampa anche parole corrette

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -5,10 +5,21 @@
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private Dictionary model;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -16,9 +27,93 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="boxLanguage"
+    // NELLA COMBO BOX QUA IMPORTANDO COMPARE <?>
+    // IO VOGLIO UNA STRINGA QUINDI SOSTITUISCO CON "<STRING>"
+    private ComboBox<String> boxLanguage; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtInserisci"
+    private TextArea txtInserisci; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnSpellCheck"
+    private Button btnSpellCheck; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtRisultato"
+    private TextArea txtRisultato; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtErrori"
+    private TextField txtErrori; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnClearText"
+    private Button btnClearText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTime"
+    private TextField txtTime; // Value injected by FXMLLoader
+
+    @FXML
+    void handleClearText(ActionEvent event) {
+    	
+    	
+
+    }
+
+    @FXML
+    void handleSpellCheck(ActionEvent event) {
+    	
+    	long startTime = System.nanoTime();
+    	
+    	String inserisci = txtInserisci.getText().toLowerCase();
+    	inserisci = inserisci.replaceAll("[.,\\/#!$%?\\^&\\*;:{}=\\-_'()\\[\\]\"]", "");
+    	
+    	// --> controllo diverso da null
+    	
+    	
+    	// divido quello che viene passato come parametro
+    	// e ogni parola diventa un elemento string della lista (listaInput)
+    	String [] array = inserisci.split(" ");
+    	List<String> listaInput = new LinkedList<>();
+    	
+    	for (String s: array) {
+    		listaInput.add(s);
+    	}
+    	
+    	// this.model.spellCheckText(listaInput);
+    	
+    	
+    	List <String> listaFinale = new LinkedList<>();
+    	listaFinale = this.model.controlloParoleSbagliate(this.model.spellCheckText(listaInput));
+    	String risultato ="";
+    	for (String s: listaFinale) {
+    		risultato += s + "\n";
+    	}
+    	
+    	long endTime = System.nanoTime();
+    	
+    	txtRisultato.setText(risultato);
+    	txtErrori.setText("The text contains "+listaFinale.size()+" errors");
+    	txtTime.setText("Spell check completed in "+(endTime-startTime)+" seconds");
+    	
+
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert boxLanguage != null : "fx:id=\"boxLanguage\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtInserisci != null : "fx:id=\"txtInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnSpellCheck != null : "fx:id=\"btnSpellCheck\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtErrori != null : "fx:id=\"txtErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'Scene.fxml'.";
 
+    }
+    
+    public void setModel(Dictionary model) {
+    	this.model = model;
+    	String languages [] = {"English", "Italian"};
+    	boxLanguage.getItems().addAll(languages);
+    	// SE adAll è sottolineato andare a cambiare 
+    	// nella dichiarazione del box è mettere <String>
     }
 }
 
---
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -0,0 +1,88 @@
+package it.polito.tdp.spellchecker.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Dictionary {
+	
+	List <String> dizionario = new LinkedList<>();
+	
+	public void loadDictionary(String language) {
+		
+		if (language.equals("Italian")) {
+			
+			try {
+				
+				FileReader fr = new FileReader ("src/main/resources/Italian.txt");
+				BufferedReader br = new BufferedReader(fr);
+				String word;
+				while ((word = br.readLine()) != null) {
+					dizionario.add(word);
+					
+				}
+				
+				br.close();
+			}catch (IOException e) {
+				System.out.println("Errore nella lettura da file");
+			}
+		}
+		
+		else if (language.equals("English")) {
+			
+			try {
+				
+				FileReader fr = new FileReader ("src/main/resources/English.txt");
+				BufferedReader br = new BufferedReader(fr);
+				String word;
+				while ((word = br.readLine()) != null) {
+					dizionario.add(word);
+					
+				}
+				
+				br.close();
+			}catch (IOException e) {
+				System.out.println("Errore nella lettura da file");
+			}
+			
+		}
+	}
+	
+	// CREO UN METODO CHE CREA UNA LISTA CON TUTTE LE PAROLE 
+	// SCRITTE DALL'UTENTE, SIA GIUSTE (true) CHE SBAGLIATE (false)
+	public List <RichWord> spellCheckText(List<String> inputTextList){
+		
+		List<RichWord> paroleScritte = new LinkedList<>();
+		
+		for (String s : inputTextList) {
+			if (this.dizionario.contains(s)) {
+				RichWord r = new RichWord (s, true);
+				paroleScritte.add(r);
+				
+			}
+			else {
+				RichWord r = new RichWord (s, false);
+				paroleScritte.add(r);
+			}
+		}
+		return paroleScritte;
+		
+	}
+	
+	// CREO UN METODO CHE MI RESTITUISCE LE PAROLE SBAGLIATE
+	// A PARTIRE DALLE PAROLE SCRITTE DALL'UTENTE
+	public List <String> controlloParoleSbagliate(List<RichWord> paroleScritte){
+		
+		List <String> listaParoleSbagliate = new LinkedList<>(); 
+		
+		for (RichWord r : paroleScritte) {
+			if (r.isCorretta() == false) {
+				listaParoleSbagliate.add(r.getParola());
+			}
+		}
+		return listaParoleSbagliate;
+	}
+
+}
